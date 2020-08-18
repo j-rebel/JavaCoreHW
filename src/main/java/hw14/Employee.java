cacb2fb83ee3808bcb200fdf7051f021c5896bef
Panin Nikolai <moskowkm@gmail.com>
2020-08-18 12:57:35 +0300

    tasks 14-16 done

---
+++ src/main/java/hw14/Employee.java
@@ -0,0 +1,22 @@
+package hw14;
+
+import com.opencsv.bean.CsvBindByName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @CsvBindByName
+    public long id;
+    @CsvBindByName
+    public String firstName;
+    @CsvBindByName
+    public String lastName;
+    @CsvBindByName
+    public String country;
+    @CsvBindByName
+    public int age;
+}
